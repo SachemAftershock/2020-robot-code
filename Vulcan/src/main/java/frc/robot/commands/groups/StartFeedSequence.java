5f27a3e0864b472ba91e7169d722b9f8f53808f1
shreyasprasad14 <shreyasprasad14@gmail.com>
2020-02-27 22:35:55 -0500

    Initial Experimental Commit

--- Vulcan/src/main/java/frc/robot/commands/groups/StartFeedSequence.java
+++ Vulcan/src/main/java/frc/robot/commands/groups/StartFeedSequence.java
@@ -3,7 +3,6 @@ package frc.robot.commands.groups;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.commands.superstructure.intake.DeployIntakeCommand;
 import frc.robot.commands.superstructure.intake.IngestIntakeCommand;
-import frc.robot.commands.superstructure.storage.OpenChamberValveCommand;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.StorageSubsystem;
 
@@ -12,8 +11,7 @@ public class StartFeedSequence extends ParallelCommandGroup {
     public StartFeedSequence(IntakeSubsystem intake, StorageSubsystem storage) {
         addCommands(
             new IngestIntakeCommand(intake),
-            new DeployIntakeCommand(intake),
-            new OpenChamberValveCommand(storage)
+            new DeployIntakeCommand(intake)
         );
     }
 }
